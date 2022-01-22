2ec5782ca3a1721bc8422f850bff3ce55de034d7
Devesh Jain <76024137+devesh2511@users.noreply.github.com>
2022-01-22 12:08:49 +0530

    Create remove element.cpp

---
+++ 0027-Remove-Element/remove element.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int removeElement(vector<int>& nums, int val)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != val)
+            {
+                if (i != k)
+                    nums[k++] = nums[i];
+                else
+                    ++k;
+            }
+        }
+        return k;
+    }
+};
